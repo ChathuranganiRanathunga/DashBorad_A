367b486931d7adfa6b3eeb655f44e80463855b92
Chathurangani Ranathunga <66179809+ChathuranganiRanathunga@users.noreply.github.com>
2023-07-07 12:40:52 +0530

    Previous Next and Finish button added to the registration form-Fully functional

--- JS/RegistrationForm.js
+++ JS/RegistrationForm.js
@@ -29,3 +29,50 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 });
+
+//For Next,Previous and finish buttons
+document.addEventListener("DOMContentLoaded", function () {
+  const steps = document.querySelectorAll(".step");
+  const stepContents = document.querySelectorAll(".step-content");
+  const prevButton = document.querySelector(".prev-button");
+  const nextButton = document.querySelector(".next-button");
+  const finishButton = document.querySelector(".finish-button");
+
+  let currentStep = 0;
+
+  showStep(currentStep);
+
+  prevButton.addEventListener("click", function () {
+    currentStep--;
+    showStep(currentStep);
+  });
+
+  nextButton.addEventListener("click", function () {
+    currentStep++;
+    showStep(currentStep);
+  });
+
+  function showStep(stepIndex) {
+    stepContents.forEach(function (stepContent, index) {
+      if (index === stepIndex) {
+        stepContent.style.display = "block";
+      } else {
+        stepContent.style.display = "none";
+      }
+    });
+
+    steps.forEach(function (step, index) {
+      if (index < stepIndex) {
+        step.classList.add("completed");
+      } else {
+        step.classList.remove("completed");
+      }
+    });
+
+    prevButton.disabled = stepIndex === 0;
+    nextButton.style.display =
+      stepIndex === stepContents.length - 1 ? "none" : "inline-block";
+    finishButton.style.display =
+      stepIndex === stepContents.length - 1 ? "inline-block" : "none";
+  }
+});
